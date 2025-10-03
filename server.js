e969783a7d336c7dd6d81f56ebfc56fde7653444
rnfyatendra-lab <rnf.yatendra@gmail.com>
2025-10-03 08:28:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const RedisStore = require("connect-redis").default;
+const Redis = require("ioredis");
 
-app.post("/send-mail", async (req, res) => {
-  try {
-    const { senderName, senderEmail, subject, message, recipients } = req.body;
+let redisClient = new Redis(process.env.REDIS_URL);
 
-    const msg = {
-      to: recipients.split(","),
-      from: { name: senderName, email: senderEmail }, 
-      subject: subject,
-      text: message
-    };
-
-    await sgMail.sendMultiple(msg);
-    res.json({ success: true, message: "Mail sent successfully via SendGrid!" });
-  } catch (err) {
-    res.json({ success: false, message: "Mail sending failed: " + err.message });
-  }
-});
+app.use(session({
+  store: new RedisStore({ client: redisClient }),
+  secret: "fastmail_secret",
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: true }
+}));
