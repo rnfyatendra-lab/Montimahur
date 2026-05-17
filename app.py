1e3b070387fcbb043ae44cad6e20368474735350
rnfyatendra-lab <rnf.yatendra@gmail.com>
2026-05-17 10:20:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        # SAME LOGIN + PASSWORD
         if username == "&&&&" and password == "&&&&":
 
             session["user"] = username
@@ -26,14 +25,25 @@ def login():
     return render_template("login.html")
 
 
-# MAILER PAGE
+# MAILER
 @app.route("/launcher", methods=["GET", "POST"])
 def launcher():
 
     if "user" not in session:
         return redirect(url_for("login"))
 
+    # DEFAULT VALUES
+    data = {
+        "sender_name": "",
+        "gmail": "",
+        "app_password": "",
+        "subject": "",
+        "body": "",
+        "recipients": ""
+    }
+
     message = ""
+    total_sent = 0
 
     if request.method == "POST":
 
@@ -44,23 +54,36 @@ def launcher():
         body = request.form.get("body")
         recipients = request.form.get("recipients")
 
+        # SAVE OLD DATA
+        data = {
+            "sender_name": sender_name,
+            "gmail": gmail,
+            "app_password": app_password,
+            "subject": subject,
+            "body": body,
+            "recipients": recipients
+        }
+
         try:
 
-            # EMAIL LIST
             emails = []
 
+            # SPLIT EMAILS
             for line in recipients.splitlines():
 
                 if "," in line:
+
                     parts = line.split(",")
 
                     for p in parts:
+
                         p = p.strip()
 
                         if p:
                             emails.append(p)
 
                 else:
+
                     line = line.strip()
 
                     if line:
@@ -112,7 +135,9 @@ def launcher():
 
             server.quit()
 
-            message = f"Successfully sent {sent} emails"
+            total_sent = sent
+
+            message = f"Send {sent}"
 
         except Exception as e:
 
@@ -120,7 +145,9 @@ def launcher():
 
     return render_template(
         "launcher.html",
-        message=message
+        message=message,
+        total_sent=total_sent,
+        data=data
     )
 
 
